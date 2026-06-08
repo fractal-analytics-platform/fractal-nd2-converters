aa3fbac81c1143ea04eca0c076c0b3bc5d0acae0
Flurin Sturzenegger <st.flurin@gmail.com>
2026-06-08 11:49:23 +0200

    fix cross-platform bug in tests

--- tests/utils.py
+++ tests/utils.py
@@ -209,7 +209,9 @@ def _check_roi_tables(
             if roi_assert.yx_origin is not None:
                 y_origin = getattr(roi, "y_micrometer_original", None)
                 x_origin = getattr(roi, "x_micrometer_original", None)
-                assert [y_origin, x_origin] == roi_assert.yx_origin
+                assert np.allclose(
+                    [y_origin, x_origin], roi_assert.yx_origin
+                ), [y_origin, x_origin]
 
 
 def _check_image_against_assertion(
